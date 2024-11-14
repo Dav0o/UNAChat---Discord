49fce045cc81511686e40581ff8936d34696496f
DyllanRuiz <dyllan.ruiz.vasquez@est.una.ac.cr>
2024-11-14 16:21:12 -0600

    feat: enhance ESLint configuration with security and Prettier plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,38 @@
-import globals from "globals";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginSecurity from 'eslint-plugin-security';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+	{
+		files: ['**/*.js'],
+		languageOptions: {
+			sourceType: 'commonjs',
+		},
+		plugins: {
+			prettier: pluginPrettier,
+			security: pluginSecurity,
+		},
+		rules: {
+			'prettier/prettier': ['error', { singleQuote: true, semi: true, printWidth: 100 }],
+			'security/detect-non-literal-fs-filename': 'warn',
+			'security/detect-object-injection': 'error',
+			'security/detect-eval-with-expression': 'warn',
+		},
+		ignores: ['test/**'],
+	},
+	{
+		languageOptions: {
+			globals: globals.node,
+		},
+		plugins: {
+			prettier: pluginPrettier,
+			security: pluginSecurity,
+		},
+		ignores: ['test/**'],
+	},
+	pluginJs.configs.recommended,
+	eslintConfigPrettier,
 ];
